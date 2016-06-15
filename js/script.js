b993236fed1d7481499d1f16864fcbc92a874772
Robby Kim <robert.st.kim@gmail.com>
2016-06-15 11:58:10 -0400

    up to fizzbuzztest

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+
+function fizzBuzzCount(input) {
+
+}
+
+// will return true value if number
+function fizzBuzzTest(number) {
+	// parseInt 
+	if (number === NaN) {
+		return false;
+	} else {
+		return true; 
+	}
+	// if NaN keep false / else return true
+	// modulus 1 for decimal value
+}
+
+$(document).ready(function() {
+// prompt for a number
+	var userInput = parseInt(prompt("Please enter a number"));
+// prompts to make sure its a number
+	while (fizzBuzzTest(userInput) !== true) {
+		userInput = parseInt(prompt("Not a number, Please enter a number"));
+	}
+
+// count from 1 to that number
+
+
+// replace divis by 3 with fizz
+// replace divis by 5 with buzz
+// replace divis by 3 AND 5 with fizzbuzz
+
+
+// print this all to html
+
+
+
+
+
+});
